737be765ef46ae661ca8587e44a2dd75ffc6b1b9
Christina38 <113312387+Christina38@users.noreply.github.com>
2023-03-21 19:53:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("My First Console Project")
+var first_name = "Christina";
+var last_name="Zhu";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add);
